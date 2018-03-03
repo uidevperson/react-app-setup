ea83317f88ad329254de0f56c383bf1e7d23909c
uidevperson <uidevthing@gmail.com>
2018-03-03 16:20:13 +0000

    test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // create landing page
+  // create login page
+  // create events page
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React 16.2 app setup</h1>
         </header>
         <p className="App-intro">
           testing
