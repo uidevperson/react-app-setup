d7fda06f1061df6e7a2de1377e942a8323ebf6ff
uidevperson <uidevthing@gmail.com>
2018-03-03 17:16:27 +0000

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -16,6 +16,27 @@ class App extends Component {
         <p className="App-intro">
           testing
         </p>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              1 of 2
+            </div>
+            <div class="col">
+              1 of 2
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              1 of 3
+            </div>
+            <div class="col">
+              1 of 3
+            </div>
+            <div class="col">
+              1 of 3
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
