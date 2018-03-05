9fe1177804cd518f9b3370de69f9462234760932
uidevperson <uidevthing@gmail.com>
2018-03-05 19:49:37 +0000

    init home page

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Grid, Navbar, Jumbotron, Button, Form } from 'react-bootstrap';
+import './Home.css';
+
+
+class Home extends Component {
+   
+  render() {
+    return (
+      <div>Home</div>
+    );
+  }
+}
+
+export default Home;
