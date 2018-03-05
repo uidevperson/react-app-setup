1ba8351c556be03bc35925b2e25636944f05016a
uidevperson <uidevthing@gmail.com>
2018-03-05 20:30:30 +0000

    navigation

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import News from './components/News';
+import Navbar from './components/CustomNavbar';
+
 
 class App extends Component {
   // create landing page
   // create login page
   // create events page
   // add navigation
-  
+  // https://youtu.be/jgVkR5EKI68?t=1318
   render() {
     return (
     
         <Router>
           <div>
+            <Navbar />
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/news" component={News} />
---
+++ src/components/CustomNavbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import './CustomNavBar.css';
+
+
+class CustomNavbar extends Component {
+   
+  render() {
+    return (
+      <Navbar default collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">
+              React Code
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} componentClass={Link} href='/' to='/'>
+              Home
+            </NavItem>
+            <NavItem eventKey={2} componentClass={Link} href='/' to='/about'>
+              About
+            </NavItem>
+            <NavItem eventKey={3} componentClass={Link} href='/' to='/<n></n>ews'>
+              News
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default CustomNavbar;
