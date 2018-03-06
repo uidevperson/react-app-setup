966674223c69960e09e6fa142e83a254f4729dcd
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-03-06 21:03:02 +0000

    added components, css and assets

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import News from './components/News';
+import Navbar from './components/CustomNavbar';
+
 
 class App extends Component {
   // create landing page
   // create login page
   // create events page
+  // add navigation
+  // https://youtu.be/jgVkR5EKI68?t=1318
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React 16.2 app setup</h1>
-        </header>
-        <p className="App-intro">
-          testing
-        </p>
-      </div>
+    
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/news" component={News} />
+          </div>
+        </Router>
     );
   }
 }
