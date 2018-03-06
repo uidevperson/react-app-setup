3e9ce45835a536fefd56e492629b9c57d261a07a
uidevperson <uidevthing@gmail.com>
2018-03-06 20:16:31 +0000

    update css

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -22,10 +22,10 @@ class CustomNavbar extends Component {
             <NavItem eventKey={1} componentClass={Link} href='/' to='/'>
               Home
             </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href='/' to='/about'>
+            <NavItem eventKey={2} componentClass={Link} href='/about' to='/about'>
               About
             </NavItem>
-            <NavItem eventKey={3} componentClass={Link} href='/' to='/<n></n>ews'>
+            <NavItem eventKey={3} componentClass={Link} href='/news' to='/news'>
               News
             </NavItem>
           </Nav>
